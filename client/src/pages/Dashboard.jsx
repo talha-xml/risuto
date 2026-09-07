b2027df0177b256fa990e5c845e66b364ad4ecc3
talha-xml <mtalhafaizan30@gmail.com>
2026-09-08 01:57:16 +0500

    few minor changes

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -46,7 +46,6 @@ function Dashboard() {
       <main className="dashboard">
         <DashboardHeader />
         <Analytics stats={stats} />
-        <RecentlyAdded />
       </main>
     </>
   );
